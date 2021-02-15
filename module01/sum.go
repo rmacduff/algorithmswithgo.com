7625a3fa691d9861742cd45375761070ad058202
Ross Macduff <ross@macduff.ca>
2021-02-15 17:51:28 -0500

    sum soln

--- module01/sum.go
+++ module01/sum.go
@@ -2,6 +2,18 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
+// func Sum(numbers []int) int {
+// 	var sum int
+// 	for _, n := range numbers {
+// 		sum += n
+// 	}
+// 	return sum
+// }
+
+// Recursion method
 func Sum(numbers []int) int {
-	return 0
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + Sum(numbers[1:])
 }
